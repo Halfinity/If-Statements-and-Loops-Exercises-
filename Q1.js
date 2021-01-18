1189d156fa4295b6fff81b978c37b83bd920db18
Halfinity <ofirhalfin13@gmail.com>
2021-01-18 14:15:08 +0200

    Change to alert

--- Q1.js
+++ Q1.js
@@ -4,14 +4,14 @@ num2 = window.prompt("Input the second integer", "0");
                                                    
 if(parseInt(num1, 10) > parseInt(num2, 10))   
   {   
-  console.log("The larger of "+ num1+ " and "+ num2+ " is "+ num1+ ".");  
+  alert("The larger of "+ num1+ " and "+ num2+ " is "+ num1+ ".");  
   }     
 else  
   if(parseInt(num2, 10) > parseInt(num1, 10))   
   {  
-  console.log("The larger of "+ num1+" and "+ num2+ " is "+ num2+ ".");  
+  alert("The larger of "+ num1+" and "+ num2+ " is "+ num2+ ".");  
   }                    
 else  
   {  
-   console.log("The values "+ num1+ " and "+num2+ " are equal.");  
+   alert("The values "+ num1+ " and "+num2+ " are equal.");  
   }  
