c9133486b115c7c31aeb88a3723e5774f6d9b0c1
Halfinity <ofirhalfin13@gmail.com>
2021-01-18 14:15:19 +0200

    Done Q2

---
+++ Q2.js
@@ -0,0 +1,23 @@
+var a=3;
+var b=-7;
+var c=2;
+if (a>0 && b>0 && c>0)
+{
+alert("The sign is +");
+}
+else if (a<0 && b<0 && c<0)
+{
+alert("The sign is +");
+}
+else if (a>0 && b<0 && c<0)
+{
+alert("The sign is +");
+}
+else if (a<0 && b>0 && c<0)
+{
+alert("The sign is +");
+}
+else
+{
+alert("The sign is -");
+}
